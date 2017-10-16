f785f2cf6bfd1fc387ef14b2b4f8b59ac33a1f5c
leonardogc <leonardogomescapozzi@gmail.com>
2017-10-16 17:32:32 +0100

    Add files via upload

--- AEDA_Proj1/Candidato.hpp
+++ AEDA_Proj1/Candidato.hpp
@@ -1,8 +1,21 @@
 #ifndef CANDIDATO_HPP_
 #define CANDIDATO_HPP_
+#include <string>
+#include <vector>
 
+using namespace std;
 
+class Candidato{
+private:
+	string data;
+	int id;
+	vector <Participacao *> participacao;
+public:
+	Candidato();
+	string getData();
+	int getId();
+	vector <Participacao *> getParticipacao();
 
-
+};
 
 #endif /* CANDIDATO_HPP_ */
