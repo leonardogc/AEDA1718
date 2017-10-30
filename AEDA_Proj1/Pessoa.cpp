f83485f31e833471bb0d989c75c8d03c66974776
veronicafradique <sofia_fradique@hotmail.com>
2017-10-30 15:44:06 +0000

    Methods of Pessoa

--- AEDA_Proj1/Pessoa.cpp
+++ AEDA_Proj1/Pessoa.cpp
@@ -5,3 +5,30 @@ Pessoa::Pessoa(string nome, string morada, string generoArte){
 	this->morada=morada;
 	this->generoArte=generoArte;
 }
+
+string Pessoa::getNome() const{
+	return this->nome;
+}
+
+string Pessoa::getMorada() const{
+	return this->morada;
+}
+
+string Pessoa::getGeneroArte() const{
+	return this->generoArte;
+}
+
+
+void Pessoa::setNome(string nome){
+	this->nome = nome;
+}
+
+
+void Pessoa::setMorada(string morada){
+	this->morada = morada;
+}
+
+void Pessoa::setGeneroArte(string generoArte){
+	this->generoArte = generoArte;
+}
+
